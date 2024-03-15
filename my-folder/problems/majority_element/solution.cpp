f4da26598c56f03ee8adfe0201984d26d9885837
Darsh Singh <116330695+darshsingh116@users.noreply.github.com>
2024-03-15 05:22:14 +0000

    Sync LeetCode submission - Majority Element (cpp)

---
+++ my-folder/problems/majority_element/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int element=0;
+        int frequency=0;
+        for(int i=0;i<nums.size();i++){
+            if(frequency==0){
+                element=nums[i];
+            }
+            if(element==nums[i]){
+                frequency++;
+            }
+            else{
+                frequency--;
+            }
+            
+        }
+        return element;
+    }
+};
