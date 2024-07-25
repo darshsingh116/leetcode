43420eaf6949f8d3f12641599d303948cfe0a6ee
Darsh Singh <116330695+darshsingh116@users.noreply.github.com>
2024-07-25 06:39:23 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (cpp)

--- my-folder/problems/binary_tree_postorder_traversal/solution.cpp
+++ my-folder/problems/binary_tree_postorder_traversal/solution.cpp
@@ -11,20 +11,34 @@
  */
 class Solution {
 public:
- void traverse(TreeNode* node, vector<int>& ans){
-        
-        if(node->left != nullptr){
-            traverse(node->left,ans);
-        }
-        if(node->right != nullptr){
-            traverse(node->right,ans);
-        }
-        ans.push_back(node->val);
-    }
+
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
         if(root==nullptr){return ans;}
-        traverse(root,ans);
+        stack<TreeNode*> s1;
+        stack<int>s2;
+        TreeNode* curr,temp;
+        s1.push(root);
+        while(!s1.empty()){
+            curr=s1.top();
+            s1.pop();
+            // if(curr->left == nullptr && curr->right==nullptr){
+            //     ans.push_back(curr->val);
+            // }
+            
+            if(curr->left != nullptr){
+                s1.push(curr->left);
+            }
+            if(curr->right != nullptr){
+                s1.push(curr->right);
+            }
+            s2.push(curr->val);
+
+        }
+        while(!s2.empty()){
+            ans.push_back(s2.top());
+            s2.pop();
+        }
         return ans;
     }
 };
