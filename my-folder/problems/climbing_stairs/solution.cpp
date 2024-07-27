d8fba3ff8eec6e8e1eada0abcafa347ccae4b5b6
Darsh Singh <116330695+darshsingh116@users.noreply.github.com>
2024-07-27 13:42:06 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- my-folder/problems/climbing_stairs/solution.cpp
+++ my-folder/problems/climbing_stairs/solution.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-
-    //int num=0;
-    int arr[46] = {0};
-    int func(int n){  
-        int ret=0;  
-        
-        if(n == 1){return 1;}
-        if(n == 2){return 2;}
-
-        if(n>0){
-            
-            if(arr[n-1] == 0) {arr[n-1] = func(n-1);}
-            ret = ret + arr[n-1];
-            
-            if(arr[n-2] == 0) {arr[n-2] = func(n-2);}
-            ret = ret + arr[n-2];
-        }
-       
-        
-        return ret;
-    }
     int climbStairs(int n) {
         
-        return func(n);
+        if(n==1){return 1;}
+        if(n==2){return 2;}
+        vector<int> dp(n,0);
+        dp[0]=1;dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
     }
 };
