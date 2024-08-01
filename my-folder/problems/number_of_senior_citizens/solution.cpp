cfb3110009a85d6c2e7a9557375070e6a55c83ba
Darsh Singh <116330695+darshsingh116@users.noreply.github.com>
2024-08-01 05:36:17 +0000

    Sync LeetCode submission - Number of Senior Citizens (cpp)

---
+++ my-folder/problems/number_of_senior_citizens/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans=0;
+        for(string s:details){
+            if(stoi(s.substr(11,2))>60){ans++;}
+        }
+        return ans;
+    }
+};
